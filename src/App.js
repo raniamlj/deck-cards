be2378051f7db06bfb4307ade193d61a415a3a40
raniamlj <raniamlj@gmail.com>
2018-12-10 23:27:35 +0100

    first container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './containers/Container'
-import Container from './containers/Container';
+import Container from './components/Container';
 
 class App extends Component {
  
@@ -11,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={} >Shuffle</button>
+
         <Container></Container>
         
       </div>
