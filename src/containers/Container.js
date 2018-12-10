04145ab088ef4b9abad5fb63ec5ef0a5db355807
raniamlj <raniamlj@gmail.com>
2018-12-10 19:18:44 +0100

    first container

--- src/containers/Container.js
+++ src/containers/Container.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
 
 class Container extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }    
-    shuffleCards(deck) {
-        console.log('shuffling deck');
-        let counter = deck.length;
-        let t;
-        let i;
-        
-        while (counter) {
-          i = Math.floor(Math.random() * counter-- );
-          t = deck[counter];
-          deck[counter] = deck[i];
-          deck[i] = t;
-        }
-        return deck;
-      }
 
+    state = {
+        tab: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    };
 
+    shuffleCards(tab) {
+        console.log('shuffling ');
+
+        return(tab);
+    }
 
 
     render() {
+      const tab = this.state.tab
+
         return (
-            
+            <div>
+                <button onClick={this.shuffleCards(tab)}>Shuffle</button>
+                <lu>
+                    {this.state.tab.map(elem => <tr>{elem}</tr>)}
+                </lu>
+            </div>
+
         );
     }
 }
