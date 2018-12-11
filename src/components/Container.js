b12da5ee0572aec29472a1eec1c78fded82da8c0
raniamlj <raniamlj@gmail.com>
2018-12-11 03:14:31 +0100

    first container

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "../styles/card.css"
-import "./Card"
+import Card from "./Card"
 
 class Container extends Component {
 
@@ -55,12 +55,4 @@ class Container extends Component {
 
 export default Container;
 
-const Card = (props) => {
-    if (props.suit == "♣︎" || props.suit == "♠︎") {
-      return (<div className="card card-black"><div className="card-tl"><div className="card-value">{props.value}</div><div className="card-suit">{props.suit}</div></div><div className="card-br"><div className="card-value">{props.value}</div><div className="card-suit">{props.suit}</div></div></div>);
-    } else {
-      return (<div className="card card-red"><div className="card-tl"><div className="card-value">{props.value}</div><div className="card-suit">{props.suit}</div></div>
-        <div className="card-br"><div className="card-value">{props.value}</div><div className="card-suit">{props.suit}</div></div>
-        </div>);
-    }
-  };
+
