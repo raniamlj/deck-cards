755070f7169a9c900a2b96669fdbdc8c9a355f31
raniamlj <raniamlj@gmail.com>
2018-12-11 12:48:45 +0100

    second container

--- src/components/Container.js
+++ src/components/Container.js
@@ -10,6 +10,7 @@ class Container extends Component {
         super(props);
         this.state = {};
     }
+    
 
 
     shuffleCards(deck) {
@@ -38,36 +39,64 @@ class Container extends Component {
         } 
         return card;
     }*/
-
-    dealOnePoisson(mesPoissons) {
-        console.log('deal one poisson');
-        let counter = mesPoissons.length;
-        let unPoisson;
-
-        while (counter) {
-            unPoisson = Math.floor(Math.random() * counter--);
-        }
-        return unPoisson;
-    }
-
-    retirerPoisson(mesPoissons, unPoisson) {
-        let popped = mesPoissons.pop();
-        console.log("mesPoissons après : " + mesPoissons);
-        console.log("A retiré cet élément : " + popped);
-        permutWithLast(popped,counter)
-        return mesPoissons;
-    }
-
-    permutWithLast(i,j)
+   /* permutWithLast(i,j)
     {
+        const mesPoissons = ["angel", "clown", "mandarin", "surgeon"];
         let inter=i
         mesPoissons[inter]=mesPoissons[i]
         mesPoissons[i]=mesPoissons[j]
         mesPoissons[j]=mesPoissons[inter]
+    }*/
+
+    /*retirerPoisson(mesPoissons, unPoisson) 
+    {
+        let counter = mesPoissons.length;
+        let popped = mesPoissons.pop();
+        console.log("mesPoissons après : " + mesPoissons);
+        console.log("A retiré cet élément : " + popped);
+        //permutWithLast(popped,counter)
+        let inter=popped
+        mesPoissons[inter]=mesPoissons[popped]
+        mesPoissons[popped]=mesPoissons[counter]
+        mesPoissons[counter]=mesPoissons[inter]
+
+        return mesPoissons;
+    }*/
+
+    dealOnePoisson(mesPoissons) {
+        console.log('deal one poisson');
+        let counter = mesPoissons.length;
+        console.log('longeur liste poisson', +counter);
+        let unPoisson;
+
+        unPoisson = Math.floor(Math.random() * counter--);
+        console.log("lindice du poisson selectionné : " + unPoisson);
+        console.log("le poisson selectionné : " + mesPoissons[unPoisson]);
+        //permutWithLast(popped,counter)
+        mesPoissons[inter]=mesPoissons[unPoisson]
+        mesPoissons[unPoisson]=mesPoissons[counter]
+        mesPoissons[counter]=mesPoissons[inter]
+
+         
+         let popped = mesPoissons.pop();
+         console.log("A retiré cet élément : " + popped);
+         let inter=popped
+
+ 
+        console.log("mesPoissons après : " + mesPoissons);
+        console.log("A retiré cet élément : " + popped);
+         
+       // mesPoissons = retirerPoisson(mesPoissons, unPoisson)
+       
+        
+       
+        return mesPoissons;
     }
+  
 
 
     render() {
+        const mesPoissons = ["angel", "clown", "mandarin", "surgeon"];
         const suits = ["♠︎", "♥︎", "♣︎", "♦︎"];
         const values = ["Ac", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jk", "Qn", "Kg"];
         let cardDeck = [];
@@ -83,13 +112,14 @@ class Container extends Component {
             //e.preventDefault();
             console.log('The link was clicked.');
             }*/
-        const mesPoissons = ["angel", "clown", "mandarin", "surgeon"];
+        
 
 
         return (
             <div>
-                <button onClick={this.shuffleCards.bind(this)}>Shuffle</button>
-                <button >Deal Card </button>
+                <button onClick={this.shuffleCards.bind(this)}>Shuffle</button> 
+
+                <button onClick={this.dealOnePoisson(mesPoissons)} >Deal Card </button>
 
                 <Deck cardDeck={this.shuffleCards(cardDeck)}></Deck>
 
